90fc10ffbe7c0cbbd023d31a80340615944c8356
dkazdan <30479240+dkazdan@users.noreply.github.com>
2024-12-18 20:53:49 +0000

    Add files via upload
    v. 7 has class menorah, inner class candles

--- class_Candles.6.py
+++ class_Candles.6.py
@@ -14,105 +14,55 @@ Started 12 December 2024
 DK
 """
 
-import board
 # https://docs.circuitpython.org/projects/neopixel/en/latest/api.html
-import neopixel
+import board, neopixel, sched, random
 from time import time, sleep
-import sched
 s = sched.scheduler(time, sleep)
-import random
 
-num_pixels = 8
+num_pixels = 8 # on each candle's neopixel strip
 pixel_pin = board.D18
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.GRB
 
-pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
-)
 
 class candle:
     def __init__(self,candle_number,n_pixels):
+        ORDER = neopixel.GRB # this actually produces RGB, not sure why
+        self.pixels = neopixel.NeoPixel(
+            pixel_pin, num_pixels, brightness=0.2, auto_write=True,
+            pixel_order=ORDER)
+
         self.n_pixels = n_pixels
         self.candle_number = candle_number
-        
+        self.candle_yellow = (100,75,0)
         # set pixels to dim value, show
-        self.first_pixel = self.candle_number * self.n_pixels        
-        for LED in range(self.first_pixel, self.first_pixel+self.n_pixels):
-            pixels[LED] = ((10,10,10))
-        pixels.show()
-        
+        self.first_pixel = self.candle_number * self.n_pixels
+
     def light_candle(self):
-        candle_yellow = 0x996600
+        
         flame_red = (200,0,0)
         for LED in range(self.first_pixel, self.first_pixel+self.n_pixels-1):
-            pixels[LED] = (candle_yellow)
-        pixels[self.n_pixels -1] = flame_red
-        pixels.show()
-
-def burncandle():
-    pixels.fill ((0,0,0))
-    pixels.show()
-    
-    candle_yellow = 0x996600
-    flame_red = (100,5,5)
-    pixels.fill(candle_yellow)
-    #pixels[num_pixels-1] = 0x000000
-    pixels[num_pixels-1] = flame_red
-    pixels.show()
-    
-    
-    last_wax = num_pixels-2
-    
-    while (last_wax > 0):
-        delay = random.gauss(10,.1)
-        if delay<0: delay=abs(delay)
-        sleep(delay)
-        # TODO: Make yellow dimmer toward base of candle
-        pixels[last_wax+1] = 0x000000
-        # TODO: Put sparkle/twinkle in flame
-        pixels[last_wax] = flame_red 
-        pixels.show()
-        last_wax -= 1
-    # and burnout
-    delay = random.gauss(2.5,.5)
-    if delay<0: delay=abs(delay)
-    sleep(delay)
-    # TODO: have schedule burn candle down one segment
-    pixels[0] = 0x000000
-    pixels[1] = 0x000000 
-    pixels.show()
-    
-
-def lightcandle():
-    pixels.fill((0,0,0))
-    pixels.show()
-    wax_top = num_pixels - 1
-    # start with mostly dull yellow wax
-    brightness = 50
-    color = (int(brightness*.5), int(brightness * 0.35), int(brightness * 0.1))  # Candle color (orange-ish)
-    pixels.fill(color)
-    # TODO: add randomness to top candles and to burning down.
-    pixels[num_pixels-1] = (50,50,50)
-    pixels.show()
-    sleep(5)
-    brightness = random.randint(100, 255)
-    color = (brightness, int(brightness * 0.7), int(brightness * 0.2))  # Candle color (orange-ish)
-    pixels.fill(color)
-    while True:
-        pixel_adjust = int(random.triangular(0, num_pixels-1, num_pixels-2)+1)
-        adjust = (num_pixels-pixel_adjust)/num_pixels
-        brightness = random.randint(int(50*adjust), int(255*adjust))
-        #color = (int(brightness*.5), int(brightness * 0.35), int(brightness * 0.1))  # Candle color (orange-ish)
-        color = (int(brightness*.5 * (1.-adjust/2)),
-                 int(brightness * 0.35 * adjust),
-                 int(brightness * adjust))  # Candle color (orange-ish)
-        pixels[pixel_adjust] = color
-        pixels.show()
+            self.pixels[LED] = (self.candle_yellow)
+        self.pixels[self.n_pixels -1] = ((100,0,0))
+        self.burncandle(self.n_pixels-1)
         
-        sleep(random.uniform(0.01, 0.05)) # random flicker delay
+    def burncandle(self, last_wax):
+        if (last_wax == 0):
+            self.pixels.fill((0,0,0))
+            return
+        # else
+        burn_time = random.gauss(5,1)
+        burn_time=abs(burn_time) # might have negative tail
+        sleep(burn_time)
+        last_wax -= 1
+
+        candle_yellow = 0x996600
+        flame_red = (100,5,5)
+        self.pixels[last_wax] = flame_red
+        self.pixels[last_wax +1] = ((0,0,0))
+        
+        self.burncandle(last_wax)
 
 
 if __name__ == "__main__":
@@ -124,4 +74,4 @@ if __name__ == "__main__":
         #lightcandle()
     except KeyboardInterrupt:
         pixels.fill((0,0,0))
-        pixels.show
+        pixels.show()
---
+++ class_Candles.7.py
@@ -0,0 +1,86 @@
+"""
+MUST RUN FROM ROOT
+
+Class to produce 9 inner classes of Neopixel candles.
+Neopixels need to be in a string, only one per Pi,
+so will use separate object for each candle.
+Use object 0 for shammos,
+objects 1-8 for daily hanukkah candles.
+v. 3 runs candles with some color and frequency randomness from top to bottom.
+v. 4 better candle demonstration
+v. 5 try with sched.scheduler
+v. 6 Start writing classes
+v. 7 Write candles as inner class to menorah
+Started 12 December 2024
+DK
+"""
+
+# https://docs.circuitpython.org/projects/neopixel/en/latest/api.html
+import board, neopixel, sched, random
+from time import time, sleep
+s = sched.scheduler(time, sleep)
+
+num_pixels = 8 # on each candle's neopixel strip
+pixel_pin = board.D18
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+
+class menorah:
+    def __init__(self, n_pixels_per_candle=8, n_candles=9): # 1 is shammos only
+        ORDER = neopixel.GRB # this actually produces RGB, not sure why
+        self.pixels = neopixel.NeoPixel(
+            pixel_pin, num_pixels, brightness=0.2, auto_write=True,
+            pixel_order=ORDER)
+
+        self.n_pixels = n_pixels_per_candle
+        self.candles = n_candles # total number of candles in candelabra
+        self.candle_yellow = (100,75,0)
+        self.flame_red = (200,0,0)
+        Candles=[]
+        for i in range (0, n_candles):
+            # print(i)
+            Candles.append(self.candle(self, i, 8))
+        Candles[0].light_candle()
+
+
+    class candle:
+        def __init__(self, menorah, candle_number,n_pixels):
+            self.menorah=menorah
+            self.candle_number = candle_number
+            self.n_pixels = n_pixels
+            self.first_pixel = self.candle_number * self.n_pixels
+            self.candle_yellow = (100,75,0)
+            self.flame_red = (200,0,0)
+
+        def light_candle(self):
+            
+            for LED in range(self.first_pixel, self.first_pixel+self.n_pixels-1):
+                self.menorah.pixels[LED] = self.candle_yellow
+            self.menorah.pixels[self.n_pixels -1] = self.flame_red
+            self.burncandle(self.n_pixels-1)
+            
+        def burncandle(self, last_wax): # highest point of candle
+            if (last_wax == 0): # candle is burned to stub
+                self.menorah.pixels.fill((0,0,0)) # extinguish candle
+                return
+            # else
+            burn_time = abs(random.gauss(5,1))  # might have negative tail
+            sleep(burn_time)
+            last_wax -= 1
+            self.menorah.pixels[last_wax] = self.flame_red
+            self.menorah.pixels[last_wax +1] = ((0,0,0))
+            
+            self.burncandle(last_wax)
+
+
+if __name__ == "__main__":
+    try:
+        Menorah = menorah(n_candles=1)
+        #candle0.light_candle()
+        
+        #burncandle()
+        #lightcandle()
+    except KeyboardInterrupt:
+        pixels.fill((0,0,0))
+        pixels.show()
